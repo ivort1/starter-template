344cc4a654fe374a51bef07130a14659875cd490
Ivan Ortiz <ortiz.ivan93@gmail.com>
2025-06-25 12:48:38 -0700

    Started working on the FAQ page

---
+++ assets/icons/plus.js
@@ -0,0 +1,5 @@
+const plus = `
+<svg fill="currentColor" id="fi_9312231" enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g><g><path d="m256 439.98c-13.81 0-25-11.19-25-25v-317.96c0-13.81 11.19-25 25-25s25 11.19 25 25v317.96c0 13.81-11.19 25-25 25z"></path></g><g><path d="m414.98 281h-317.96c-13.81 0-25-11.19-25-25s11.19-25 25-25h317.96c13.81 0 25 11.19 25 25s-11.19 25-25 25z"></path></g></g></svg>
+`;
+
+export default plus;
